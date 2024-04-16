0aab2176e6899fd410f244106c4f1cccd487e4ea
bonnycool <bonnydavid543@gmail.com>
2024-04-16 20:16:24 +0530

    Diagram on python

---
+++ Bonny/rect.py
@@ -0,0 +1,13 @@
+import turtle
+
+t=turtle.Turtle()
+t.speed=(6)
+t.fillcolor("blue")
+t.begin_fill()
+for _ in range(4):
+    
+                t.forward(100)
+
+                t.right(90)
+
+t.end_fill()
